232d1c064b93675a189ae875c9a28db6595a9958
Javi <encriptator3000plus@gmail.com>
2020-12-06 18:53:43 +0100

    Client login and register done

---
+++ client/src/app/home/home.component.ts
@@ -0,0 +1,24 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent implements OnInit {
+  registerMode = false; //if we don't declare the type of the variable, typescript will infer the correct type for us if we give it a value that it recognizes
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  registerToggle() {
+    this.registerMode = !this.registerMode;
+  }
+
+  cancelRegisterMode(event: boolean) {
+    this.registerMode = event;
+  }
+}
